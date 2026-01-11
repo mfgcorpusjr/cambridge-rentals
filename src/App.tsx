699f9ce384b2bf0367deaba050f830a6574c6a75
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-01-12 07:55:25 +0800

    Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "@/components/Header";
+import Title from "@/components/Title";
 
 export default function App() {
   return (
     <div>
       <Header />
+
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+export default function Title() {
+  return (
+    <div className="flex justify-center my-12">
+      <h2 className="inline-block p-4 border-b-4 border-b-primary text-3xl text-center font-semibold tracking-tight">
+        Properties for Rent
+      </h2>
+    </div>
+  );
+}
