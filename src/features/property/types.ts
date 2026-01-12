8b0c1f598b4ff72764eec6ea843907a68bfac502
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-01-12 08:12:39 +0800

    Dummy data and type

---
+++ src/features/property/types.ts
@@ -0,0 +1,12 @@
+export type Property = {
+  id: number;
+  type: string;
+  image: string;
+  bedrooms: number;
+  bathrooms: number;
+  surface: number;
+  address: string;
+  rent: number;
+  date: string;
+  available: boolean;
+};
